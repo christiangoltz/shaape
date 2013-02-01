d79fab519aac93b1d51bb84b272146eb4b0c2d45
christiangoltz <goltzchristian@googlemail.com>
2013-02-01 01:04:25 +0100

    fixed and merged planar cycle algorithm

--- shaape/graphalgorithms.py
+++ shaape/graphalgorithms.py
@@ -1,3 +1,7 @@
+import networkx as nx
+import numpy as np
+import math
+
 def reduce_path(nodes):
     new_nodes = []
     new_nodes = nodes[0:2]
@@ -35,3 +39,69 @@ def has_same_direction(v1, v2):
         return True
     else:
         return False
+
+def planar_cycles(undirected_graph):
+    def angle(v1, v2):
+        x = np.array([v1[0], v1[1]])
+        y = np.array([v2[0], v2[1]])
+        cross = np.cross(x, y)
+        dot = np.dot(x, y)
+        x_length = np.sqrt((x * x).sum())
+        y_length = np.sqrt((y * y).sum())
+        angle = np.arccos(dot / x_length / y_length)
+        return angle
+
+    def next_neighbor(_path):
+        if len(_path) == 0:
+            return graph.edges()[0][0]
+        neighbors = graph.neighbors(_path[-1])    
+        if len(neighbors) == 0:
+            return None
+        else:
+            if len(_path) == 1:
+                return neighbors[0]
+            else:
+                if _path[-2] in neighbors:
+                    neighbors.remove(_path[-2])
+                if len(neighbors) == 0:
+                    return None
+                last_dir = _path[-2] - _path[-1]
+            rightmost_nb = min(neighbors, key=lambda x: angle(last_dir, x - _path[-1]))
+            return rightmost_nb 
+
+    def contains_cycle(_cycles, _cycle):
+        for c in _cycles:
+            not_equal = False
+            if len(c) == len(_cycle):
+                if _cycle[0] in c:
+                    start = c.index(_cycle[0])
+                    for i in range(0, len(c)):
+                        if c[start - i] != _cycle[i]:
+                            not_equal = True
+                    if not_equal == False:
+                        return True
+        return False
+                    
+        
+    cycles = []
+    path = []
+    graph = undirected_graph.to_directed()
+    while graph.edges():
+        node = next_neighbor(path)
+        if node:
+            if node in path:
+                start = path[::-1].index(node)
+                start = len(path) - 1 - start
+                cycle = path[start:]
+                if not contains_cycle(cycles, cycle):
+                    cycles.append(cycle)
+                graph.remove_edge(path[-1], node)
+            else:
+                path.append(node)
+                if len(path) > 1:
+                    graph.remove_edge(path[-2], path[-1])
+        else:
+            path.pop()
+    for c in cycles:
+        c.append(c[0]) 
+    return cycles
