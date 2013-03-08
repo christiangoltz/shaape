508ef97841c3142d44dea63bff4defc560391b6b
christiangoltz <christiangoltz@googlemail.com>
2013-03-08 18:01:21 +0100

    updated unit tests to increase coverage

--- shaape/arrowparser.py
+++ shaape/arrowparser.py
@@ -19,8 +19,6 @@ class ArrowParser(Parser):
         self._arrows['^'] = UpArrow
         self._arrows['v'] = DownArrow
         return
-    def edges(self):
-        return self.__graph.edges()
 
     def run(self, raw_data, drawable_objects):
         arrows = []
@@ -40,12 +38,12 @@ class ArrowParser(Parser):
             for arrow in arrows:
                 for obj in drawable_objects:
                     connector = Node(*(arrow.connector()))
+                    # instance has to be an graph, arrows are never connected to polygons directly
                     if isinstance(obj, OpenGraph):
                         if obj.has_node(connector):
                             arrow.add_connected_object(obj)
-                    elif isinstance(obj, Polygon):
-                        if obj.frame().has_node(connector):
-                            arrow.add_connected_object(obj.frame())
+                    else:
+                        pass
             for arrow in arrows:
                 tip = Node(*(arrow.tip()))
                 nodes_in_front = [node for node in graph.nodes() if (node == tip) or angle(arrow.direction(), node - tip) <= 90]
@@ -56,14 +54,8 @@ class ArrowParser(Parser):
                         connector = Node(*(arrow.connector()))
                         path = [connector, connector + diff]
                         for obj in arrow.connected_objects():
-                            if isinstance(obj, OpenGraph):
-                                if obj.has_node(connector):
-                                    obj.add_path(path)
-                            elif isinstance(obj, Polygon):
-                                if obj.frame().has_node(connector):
-                                    obj.frame().add_path(path)
-                            else:
-                                pass
+                            # instance is always an graph, arrows are never connected to polygons directly
+                            obj.add_path(path)
                         arrow.translate(diff)
                 tip = Node(*(arrow.tip()))
                 for obj in drawable_objects:
--- shaape/font.py
+++ shaape/font.py
@@ -1,8 +1,6 @@
 class Font(object):
-    DEFAULT_NAME = 'Monospace 10'
-
-    def __init__(self):
-        self.__name = self.DEFAULT_NAME
+    def __init__(self, name = 'Monospace 10'):
+        self.__name = name
 
     def name(self):
         return self.__name
