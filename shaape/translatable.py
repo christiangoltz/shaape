aac0c16f9348a4a17fb012263abe28867ad57dcb
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-04-11 21:29:24 +0200

    Add vector class and use instead of np.array

--- shaape/translatable.py
+++ shaape/translatable.py
@@ -1,16 +1,19 @@
+from vector import Vector
+
+
 class Translatable(object):
     def __init__(self, position = (0, 0)):
-        self.__position = position
+        self.__position = Vector(position[0], position[1])
 
     def set_position(self, position):
-        self.__position = position
+        self.__position = Vector(position[0], position[1])
 
     def position(self):
         return self.__position
 
     def translate(self, diff):
-        self.__position = (self.__position[0] + diff[0], self.__position[1] + diff[1])
+        self.__position += diff
 
     def scale(self, scale):
-        self.__position = (self.__position[0] * scale[0], self.__position[1] * scale[1])
+        self.__position *= scale
         return
